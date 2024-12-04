619256f83b48e6c480a425bb622ba2803883c3c0
KalleFruitema <kallefruitema@gmail.com>
2024-12-04 07:23:16 +0100

    day_4

--- 2024/day_04/part_1.py
+++ 2024/day_04/part_1.py
@@ -1,3 +1,44 @@
-with open("2024/day_04/test_input.txt") as file:
+with open("2024/day_04/input.txt") as file:
     data = [line.strip() for line in file if line.strip() != ""]
 
+
+starts = []
+
+for y, row in enumerate(data):
+    for x, val in enumerate(row):
+        if val == "X":
+            starts.append((y, x))
+            
+directions = ((-1,-1), (-1,0), (0,-1), (-1,1), (1,-1), (1,0), (0,1), (1,1))
+
+def check_full(y, x, symbol, direction = None):
+    if symbol == "S":
+        return 1
+    position = "XMAS".index(symbol)
+    next_symbol = "XMAS"[position + 1]
+    if direction is None:
+        possible = []
+        for direction in directions:
+            ny, nx = y + direction[0], x + direction[1]
+            if 0 <= nx < len(data) and 0 <= ny < len(data) and data[ny][nx] == next_symbol:
+                possible.append((ny, nx, next_symbol, direction))
+        if len(possible) == 0:
+            return 0
+        else:
+            total = 0
+            for vals in possible:
+                total += check_full(*vals)
+            return total
+    else:
+        ny, nx = y + direction[0], x + direction[1]
+        if 0 <= nx < len(data) and 0 <= ny < len(data) and data[ny][nx] == next_symbol:
+            return check_full(ny, nx, next_symbol, direction)
+        else:
+            return 0
+ 
+total = 0
+
+for y, x in starts:
+    total += check_full(y, x, "X")
+    
+print(total)
--- 2024/day_04/part_2.py
+++ 2024/day_04/part_2.py
@@ -1,3 +1,29 @@
-with open("2024/day_04/test_input.txt") as file:
+with open("2024/day_04/input.txt") as file:
     data = [line.strip() for line in file if line.strip() != ""]
 
+starts = []
+diagonal_pairs = {
+    (-1,-1): (1,1), 
+    (-1,1): (1, -1)
+}
+opposite_char = {
+    "M": "S",
+    "S": "M"
+}
+total = 0
+
+for y, row in enumerate(data):
+    for x, val in enumerate(row):
+        if val == "A":
+            crossing = 0
+            for direction in diagonal_pairs.keys():
+                ny, nx = y + direction[0], x + direction[1]
+                opposite_direction = diagonal_pairs[direction]
+                oy, ox = y + opposite_direction[0], x + opposite_direction[1]
+                if 0 <= nx < len(data) and 0 <= ny < len(data) and (char := data[ny][nx]) in "MS":
+                    if 0 <= ox < len(data) and 0 <= oy < len(data) and data[oy][ox] == opposite_char[char]:
+                        crossing += 1
+            if crossing == 2:
+                total += 1
+
+print(total)
