0c9adb4f139d5df465f83a576316fb30b943ceb2
KalleFruitema <kallefruitema@gmail.com>
2024-12-07 14:44:34 +0100

    day_7

--- 2024/day_07/part_1.py
+++ 2024/day_07/part_1.py
@@ -1,3 +1,21 @@
-with open("2024/day_07/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+with open("2024/day_07/input.txt") as file:
+    data = [line.strip().split(":") for line in file if line.strip() != ""]
 
+total = 0
+
+for ans, vals in data:
+    ans = int(ans.strip())
+    vals = vals.strip().split()
+    
+    possible = [int(vals[0])]
+    for val in vals[1:]:
+        n_possible = []
+        for pos in possible:
+            for o in "*+":
+                n_possible.append(eval(f"{pos} {o} {val}"))
+        possible = n_possible.copy()
+    
+    if ans in possible:
+        total += ans
+        
+print(total)
