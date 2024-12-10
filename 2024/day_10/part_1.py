1f495476ef7c69939ed27f2ff69911e477d92116
KalleFruitema <kallefruitema@gmail.com>
2024-12-10 07:17:02 +0100

    day_10

--- 2024/day_10/part_1.py
+++ 2024/day_10/part_1.py
@@ -1,3 +1,36 @@
-with open("2024/day_10/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+with open("2024/day_10/input.txt") as file:
+    grid = [tuple(int(a) if a != "." else a for a in line.strip()) for line in file if line.strip() != ""]
 
+directions = ((0, -1), (1, 0), (0, 1), (-1, 0)) # N O Z W
+
+starts = []
+
+for y, row in enumerate(grid):
+    for x, val in enumerate(row):
+        if val == 0:
+            starts.append((x, y))
+            
+count = 0
+seen = set()
+def follow_path(x, y):
+    current = grid[y][x]
+    next = current + 1
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < len(grid[0]) and 0 <= ny < len(grid) and grid[ny][nx] == next and (nx, ny) not in seen:
+            seen.add((nx, ny))
+            if next == 9:
+                global count
+                count += 1
+            else:
+                follow_path(nx, ny)
+    
+total = 0
+    
+for sx, sy in starts:
+    follow_path(sx, sy)
+    total += count
+    seen.clear()
+    count = 0
+    
+print(total)
