d3b1495666d56d255d17b9ac20994ebdca0fb38a
KalleFruitema <kallefruitema@gmail.com>
2024-12-23 15:03:27 +0100

    day_23

--- 2024/day_23/part_2.py
+++ 2024/day_23/part_2.py
@@ -1,3 +1,31 @@
-with open("2024/day_23/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+from collections import defaultdict
 
+
+with open("2024/day_23/input.txt") as file:
+    data = [line.strip().split("-") for line in file if line.strip() != ""]
+
+connections = defaultdict(lambda: set())
+
+for a, b in data:
+    connections[a].add(b)
+    connections[b].add(a)
+
+networks = set()
+
+
+def find_connection(comp, conns):
+    current = tuple(sorted(conns))
+    if current in networks:
+        return
+    networks.add(current)
+    
+    for comp2 in connections[comp]:
+        if comp2 in conns or not conns <= connections[comp2]:
+            continue
+        find_connection(comp2, conns | {comp2})
+
+
+for comp in connections.keys():
+    find_connection(comp, {comp})
+
+print(",".join(max(networks, key=len)))
