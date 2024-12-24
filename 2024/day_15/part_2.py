beea9d63612fb3558e1ab8a0f727cf7869f84469
KalleFruitema <kallefruitema@gmail.com>
2024-12-24 17:15:59 +0100

    day_15

--- 2024/day_15/part_2.py
+++ 2024/day_15/part_2.py
@@ -1,26 +1,13 @@
-with open("2024/day_15/test3_input.txt") as file:
-    ogrid, moves = file.read().strip().split("\n\n")
-    grid = []
-    for line in ogrid.split("\n"):
-        n_line = []
-        for char in line.strip():
-            if char == "O":
-                n_line.extend("[]")
-            elif char == "@":
-                n_line.extend("@.")
-            else:
-                n_line.extend(char*2)
-        grid.append(n_line)
+from copy import deepcopy
+
+
+with open("2024/day_15/input.txt") as file:
+    grid, moves = file.read().strip().split("\n\n")
+    grid = [list(line.strip()) for line in grid.split("\n")]
     moves = moves.strip().replace("\n", "")
 
-
-
-def gprint(grid):
-    print()
-    for line in grid:
-        print(" ".join(str(i) for i in line))
-    print()
-      
+expansion = {"#": "##", "O": "[]", ".": "..", "@": "@."}
+grid = [list("".join(expansion[char] for char in line)) for line in grid]
 
 move_dict = {
     "^": (0, -1),
@@ -29,23 +16,40 @@ move_dict = {
     "<": (-1, 0)
 }
 
-gprint(grid)
+for y, row in enumerate(grid):
+    for x, val in enumerate(row):
+        if val == "@":
+            start = (x, y)
 
+x, y = start
+for move in moves:
+    dx, dy = move_dict[move]
+    targets = [(x, y)]
+    good = True
+    for tx, ty in targets:
+        nx, ny = tx + dx, ty + dy
+        if (nx, ny) in targets:
+            continue
+        char = grid[ny][nx]
+        if char == "#":
+            good = False
+            break
+        elif char == "[":
+            targets.append((nx, ny))
+            targets.append((nx + 1, ny))
+        elif char == "]":
+            targets.append((nx, ny))
+            targets.append((nx - 1, ny))
+    if not good:
+        continue
+    grid2 = [list(row) for row in grid]
+    grid[y][x] = "."
+    grid[y + dy][x + dx] = "@"
+    for bx, by in targets[1:]:
+        grid[by][bx] = "."
+    for bx, by in targets[1:]:
+        grid[by + dy][bx + dx] = grid2[by][bx]
+    x += dx
+    y += dy
 
-def look_ahead(x, y, dx, dy, boxes: list[tuple[int, int]]):
-    boxes.append((x, y))
-    for ex in range(0, 2):
-        nx1, ny1 = x + dx, y + dy
-        nx2, ny2 = x + dx + ex, y + dy + ex
-        if grid[ny1][nx1] == "#" or grid[ny2][nx2] == "#":
-            return -1
-        elif grid[ny1][nx1] == "[":
-            return look_ahead(nx1, ny1, dx, dy, boxes)
-        elif grid[ny2][nx2] == "[":
-            return look_ahead(nx2, ny2, dx, dy, boxes)
-        elif grid[ny1][nx1] == "]":
-            return look_ahead(nx1 - 1, ny1 - 1, dx, dy, boxes)
-        elif grid[ny2][nx2] == "]":
-            return look_ahead(nx2 - 1, ny2 - 1, dx, dy, boxes)
-        elif grid[ny1][nx1] == "." or grid[ny2][nx2] == ".":
-            return boxes
+print(sum(y * 100 + x for y, row in enumerate(grid) for x, val in enumerate(row) if val == "["))
