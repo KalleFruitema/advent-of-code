e284a24fcf6ab00fa6f77c2a6ff9fe88d8044c28
KalleFruitema <kallefruitema@gmail.com>
2024-12-25 12:55:15 +0100

    day_25 p1

--- 2024/day_25/part_1.py
+++ 2024/day_25/part_1.py
@@ -1,3 +1,47 @@
-with open("2024/day_25/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+from pprint import pprint
 
+
+with open("2024/day_25/input.txt") as file:
+    data = [i.strip().split("\n") for i in file.read().strip().split("\n\n")]
+
+locks = []
+keys = []
+
+for i in data:
+    if all(x == "#" for x in i[0]):
+        locks.append(i)
+    else:
+        keys.append(i)
+
+lock_heights = []
+
+for lock in locks:
+    heights = []
+    for col in tuple(zip(*lock)):
+        for y, val in enumerate(col):
+            if val != "#":
+                heights.append(y - 1)
+                break
+    lock_heights.append(heights)
+    
+key_heights = []
+for key in keys:
+    heights = []
+    for col in tuple(zip(*key)):
+        for y, val in enumerate(col):
+            if val == "#":
+                heights.append(y)
+                break
+    key_heights.append(heights)
+
+total = 0
+
+for lock in lock_heights:
+    for key in key_heights:
+        for l, k in zip(lock, key):
+            if l >= k:
+                break
+        else:
+            total += 1
+
+print(total)
