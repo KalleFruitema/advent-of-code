a9002d07f090434c71144dde9f6f7e8fa5c59575
KalleFruitema <kallefruitema@gmail.com>
2025-12-02 13:08:13 +0100

    2025 day_02

--- 2025/day_02/part_1.py
+++ 2025/day_02/part_1.py
@@ -1,3 +1,16 @@
-with open("2025/day_02/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+with open("input.txt") as file:
+    data = [tuple(map(int, r.split("-"))) for r in file.read().strip().split(",")]
+    data = [range(i[0], i[1] + 1) for i in data]
 
+invalid = []
+
+for r in data:
+    for i in r:
+        y = str(i)
+        if len(y) % 2 != 0:
+            continue
+        a = len(y) // 2
+        if y[:a] == y[a:]:
+            invalid.append(i)
+
+print(sum(invalid))
--- 2025/day_02/part_2.py
+++ 2025/day_02/part_2.py
@@ -1,3 +1,22 @@
-with open("2025/day_02/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+with open("input.txt") as file:
+    data = [tuple(map(int, r.split("-"))) for r in file.read().strip().split(",")]
+    data = [range(i[0], i[1] + 1) for i in data]
 
+invalid = []
+
+for r in data:
+    for i in r:
+        y = str(i)
+        for split in range(1, len(y) // 2 + 1):
+            if not (len(y) / split).is_integer():
+                continue
+            s = y[:split]
+            for times in range(1, (len(y) // split)):
+                if s != y[times * split:(times+1) * split]:
+                    break
+            else:
+                invalid.append(i)
+                break
+            
+
+print(sum(invalid))
