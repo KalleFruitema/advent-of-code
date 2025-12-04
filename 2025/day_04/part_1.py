4af223149b6f52669dd4bf5d6f5a6d133f77368c
KalleFruitema <kallefruitema@gmail.com>
2025-12-04 15:11:56 +0100

    2025 day_04

--- 2025/day_04/part_1.py
+++ 2025/day_04/part_1.py
@@ -1,3 +1,29 @@
-with open("2025/day_04/test_input.txt") as file:
+with open("input.txt") as file:
     data = [line.strip() for line in file if line.strip() != ""]
 
+adjacent = (
+    (-1, -1), (-1, 0), (-1, 1),
+    (0, -1), (0, 1),
+    (1, -1), (1, 0), (1, 1)
+)
+
+accessible = 0
+
+for y, line in enumerate(data):
+    for x, char in enumerate(line):
+        if char == '@':
+            count = 0
+            for ay, ax in adjacent:
+                ny = y + ay
+                nx = x + ax
+                if not (0 <= ny < len(data) and 0 <= nx < len(line)):
+                    continue
+                elif data[ny][nx] == "@":
+                    count += 1
+                    
+                if count >= 4:
+                    break
+            else:
+                accessible += 1
+
+print(accessible)
--- 2025/day_04/part_2.py
+++ 2025/day_04/part_2.py
@@ -1,3 +1,42 @@
-with open("2025/day_04/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+with open("input.txt") as file:
+    data = [list(line.strip()) for line in file if line.strip() != ""]
 
+adjacent = (
+    (-1, -1), (-1, 0), (-1, 1),
+    (0, -1), (0, 1),
+    (1, -1), (1, 0), (1, 1)
+)
+
+total = 0
+
+while True:
+    locs = []
+    accessible = 0
+
+    for y, line in enumerate(data):
+        for x, char in enumerate(line):
+            if char == '@':
+                count = 0
+                for ay, ax in adjacent:
+                    ny = y + ay
+                    nx = x + ax
+                    if not (0 <= ny < len(data) and 0 <= nx < len(line)):
+                        continue
+                    elif data[ny][nx] == "@":
+                        count += 1
+                        
+                    if count >= 4:
+                        break
+                else:
+                    locs.append((y, x))
+                    accessible += 1
+    
+    if accessible == 0:
+        break
+    
+    for y, x in locs:
+        data[y][x] = "."
+    
+    total += accessible
+
+print(total)
