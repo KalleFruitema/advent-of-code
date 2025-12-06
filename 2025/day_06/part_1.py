463db060c9490b5b0017014a237a0b4dad7b5059
KalleFruitema <kallefruitema@gmail.com>
2025-12-06 16:19:35 +0100

    2025 day_06

--- 2025/day_06/part_1.py
+++ 2025/day_06/part_1.py
@@ -1,3 +1,19 @@
-with open("2025/day_06/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+from functools import reduce
+from operator import mul, add
 
+
+with open("input.txt") as file:
+    data = [line.strip() for line in file if line.strip() != ""]
+    operators = data.pop().split()
+    nums_list = tuple(zip(*[map(int, line.split()) for line in data]))
+    
+
+total = 0
+
+for operator, nums in zip(operators, nums_list):
+    if operator == "*":
+        total += reduce(mul, nums)
+    else:
+        total += reduce(add, nums)
+
+print(total)
--- 2025/day_06/part_2.py
+++ 2025/day_06/part_2.py
@@ -1,3 +1,28 @@
-with open("2025/day_06/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+from functools import reduce
+from operator import mul, add
+import numpy as np
 
+
+with open("input.txt") as file:
+    data = [line for line in file if line.strip() != ""]
+    operators = data.pop()
+    nums_list = np.array([list(i) for i in data])
+
+curr_nums = []
+curr_op = operators[0]
+total = 0
+
+for i, op in enumerate(operators):
+    if not op.isspace() and i != 0 or i == len(operators) - 1:
+        nums = [int(num) for i in curr_nums if (num := ''.join(i).strip()) != '']
+        if curr_op == "*":
+            total += reduce(mul, nums)
+        else:
+            total += reduce(add, nums)
+        
+        curr_nums = [nums_list[:, i]]
+        curr_op = op
+    else:
+        curr_nums.append(nums_list[:, i])
+
+print(total)
