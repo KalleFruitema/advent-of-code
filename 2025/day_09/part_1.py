0d2a0aea955eeeac4f9d163c91ee7928b3786bd6
KalleFruitema <kallefruitema@gmail.com>
2025-12-09 16:52:20 +0100

    2025 day_09

--- 2025/day_09/part_1.py
+++ 2025/day_09/part_1.py
@@ -1,3 +1,11 @@
-with open("2025/day_09/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+with open("input.txt") as file:
+    points = [tuple(map(int, line.strip().split(","))) for line in file if line.strip()]
 
+largest = 0
+
+for i, (x1, y1) in enumerate(points):
+    for x2, y2 in points[i+1:]:
+        surface = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+        largest = max(largest, surface)
+
+print(largest)
--- 2025/day_09/part_2.py
+++ 2025/day_09/part_2.py
@@ -1,3 +1,18 @@
-with open("2025/day_09/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+from shapely import Polygon
 
+
+with open("input.txt") as file:
+    points = [tuple(map(int, line.strip().split(","))) for line in file if line.strip()]
+
+allowed_area = Polygon(points)
+
+largest = 0
+for i, (x1, y1) in enumerate(points):
+    for x2, y2 in points[i+1:]:
+        rect = Polygon(((x1, y1), (x2, y1), (x2, y2), (x1, y2)))
+        
+        if allowed_area.contains(rect):
+            surface = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+            largest = max(largest, surface)
+
+print(largest)
