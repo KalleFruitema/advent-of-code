c78515962d4aa09d63eae7391556966fe872e8e2
KalleFruitema <kallefruitema@gmail.com>
2025-12-11 18:02:33 +0100

    2025 day_11

--- 2025/day_11/part_1.py
+++ 2025/day_11/part_1.py
@@ -1,3 +1,25 @@
-with open("2025/day_11/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+from collections import deque
 
+
+with open("input.txt") as file:
+    data = [line.strip().split(":") for line in file if line.strip()]
+    
+edges = {}
+for start, ends in data:
+    edges[start.strip()] = tuple(i.strip() for i in ends.split())
+
+
+pq = deque(["you"])
+count = 0
+
+while pq:
+    node = pq.popleft()
+
+    if node == "out":
+        count += 1
+        continue
+    
+    for n_node in edges[node]:
+        pq.append(n_node)
+
+print(count)
--- 2025/day_11/part_2.py
+++ 2025/day_11/part_2.py
@@ -1,3 +1,28 @@
-with open("2025/day_11/test_input.txt") as file:
-    data = [line.strip() for line in file if line.strip() != ""]
+from functools import lru_cache
 
+
+with open("input.txt") as file:
+    data = [line.strip().split(":") for line in file if line.strip()]
+    
+edges = {}
+for start, ends in data:
+    edges[start.strip()] = tuple(i.strip() for i in ends.split())
+
+
+@lru_cache
+def count_paths(node, end):
+    if node == end:
+        return 1
+
+    total = 0
+    for n_node in edges.get(node, []):
+        total += count_paths(n_node, end)
+    return total
+
+
+if count_paths("fft", "dac"):
+    paths = count_paths("svr", "fft") * count_paths("fft", "dac") * count_paths("dac", "out")
+else:
+    paths = count_paths("svr", "dac") * count_paths("dac", "fft") * count_paths("fft", "out")
+
+print(paths)
